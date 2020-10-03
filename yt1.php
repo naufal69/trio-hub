36d5c8cd2e6b2b32232ac9a7419785c2159d0e19
Naufal Dzakwan <72286813+naufal69@users.noreply.github.com>
2020-10-03 18:59:35 +0700

    Create yt1.php

---
+++ yt1.php
@@ -0,0 +1,46 @@
+
+	
+	<?php
+	include("head.php");
+	?>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Youtube Downloader</h2>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                <hr />
+                <div class="row">
+						<div class="col-md-9">
+							<form action="yt2.php" method="GET" class="form-inline my-1 my-lg-1">
+							<input class="form-control active-cyan-4 mb-4" type="text" name="yt" placeholder="https://www.youtube.com/watch?v=mQ9zH9hH_bo" value="">
+							<button class="btn btn-secondary my-2 btn-danger my-sm-0" type="submit">Download</button>
+							</form>
+						</div>
+            </div>
+                </div>
+                <!-- /. ROW  -->
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
