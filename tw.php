5c70d4f3505f4fca8cb539308fdfe0b65e0c9809
Naufal Dzakwan <72286813+naufal69@users.noreply.github.com>
2020-10-03 19:01:59 +0700

    Create tw.php

---
+++ tw.php
@@ -0,0 +1,43 @@
+
+	
+	<?php
+	include("head.php");
+	?>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper">
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>Twitter Downloader</h2>
+                    </div>
+                </div>
+                <!-- /. ROW  -->
+                <hr />
+                <div class="row">
+				<center>
+				<iframe frameBorder="0" src="https://mp3downy.com/twitter-video-downloader?apikey=&color=17a2b8&bg=ffffff" style="width:90%;min-height:250px;height:auto;" ></iframe>
+				</center>
+				</div>
+                </div>
+                <!-- /. ROW  -->
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+    <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+    <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+
+
+</body>
+</html>
